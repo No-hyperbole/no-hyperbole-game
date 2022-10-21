7ae9b5bf1b862d9c579d1fdc0cd225fabcd663c2
Adikko <adriankulik93@gmail.com>
2022-10-21 20:01:29 +0200

    the splashScreen doesn't randomly appear

--- js/splashScreenSlideOut.js
+++ js/splashScreenSlideOut.js
@@ -1,6 +1,6 @@
 function splashScreenSlideOut() {
   let splash = document.getElementsByClassName("splashScreen")[0];
-  splash.classList.toggle("splashScreen__slideOut");
+  splash.style.bottom = "100%";
 }
 
 // if the splash screen isn't clicked/tapped, it moves out of the way after 5 seounds
